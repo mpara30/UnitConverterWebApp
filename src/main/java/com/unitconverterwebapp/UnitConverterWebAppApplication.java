a3aee837356d43cf8b340117d30939fd7e83912c
mpara30 <para.mihai99@gmail.com>
2024-05-31 11:00:23 +0300

    Migration from tomcat to spring

---
+++ src/main/java/com/unitconverterwebapp/UnitConverterWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.unitconverterwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitConverterWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnitConverterWebAppApplication.class, args);
+    }
+
+}
