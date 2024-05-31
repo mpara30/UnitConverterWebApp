5f1812bceb834ff17bb298d6f13ca080543768eb
mpara30 <para.mihai99@gmail.com>
2024-05-31 11:08:07 +0300

    Migration from tomcat to spring

---
+++ src/test/java/com/unitconverterwebapp/UnitConverterWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unitconverterwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitConverterWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
